903f652b87645029343e3cf923b54cf3c8fce988
faizanshabir123 <faizanshabir69@gmail.com>
2023-07-23 15:33:49 +0500

    Create Lecture #7 Ternary operation.cpp

---
+++ Semester 1 Fall 2022/CS1002-Programming Fundamentals/C++ Programs (Course)/Lecture #7 Ternary operation/Lecture #7 Ternary operation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// ternary operation
+int main()
+{
+	int age;
+	cout << "Enter age: " << endl;
+	cin >> age;
+
+	(age >= 18) ? cout << "Eligible" << endl : cout << "Not eligible" << endl;
+	//condition   //then/true/if               //else 
+}
