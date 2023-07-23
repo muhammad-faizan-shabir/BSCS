e57df5033e673f897ebbc519091059b0adc9b7fc
faizanshabir123 <faizanshabir69@gmail.com>
2023-07-23 15:33:27 +0500

    Create Lecture #6 Decision Making; entered number.cpp

---
+++ Semester 1 Fall 2022/CS1002-Programming Fundamentals/C++ Programs (Course)/Lecture #6 Decision Making; entered number not 5 or 6/Lecture #6 Decision Making; entered number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	cout << "enter a number" << endl;
+	cin >> num;
+	if ((num != 5) && (num != 6)) 
+	{
+		cout << "Num is not 5 or 6" << endl;
+	}
+	else 
+	{
+		cout << "Num is 5 or 6" << endl;
+	}
+}
