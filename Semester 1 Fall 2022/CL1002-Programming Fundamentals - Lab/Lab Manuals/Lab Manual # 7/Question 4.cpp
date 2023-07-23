a28b09ce7c671bd731e13e82d08026ec111faa20
faizanshabir123 <faizanshabir69@gmail.com>
2023-07-23 15:13:55 +0500

    Create Question 4.cpp

---
+++ Semester 1 Fall 2022/CL1002-Programming Fundamentals - Lab/Lab Manuals/Lab Manual # 7/Question 4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	
+	char alphabets[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "ASCCI of small " << alphabets[i] << " is: " << i + 97 << endl;
+	}
+
+	
+
+
+	
+}
