11014cf18e3732ecba733b3f609011444b73b2b3
faizanshabir123 <faizanshabir69@gmail.com>
2023-07-23 15:21:27 +0500

    Create Question 4.cpp

---
+++ Semester 1 Fall 2022/CS1002-Programming Fundamentals/Assignments/PF Assignment 2/Question 4/Question 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "BBBBBB     JJJJJ    AAAAA    RRRRR    N    N    EEEEE" << endl;
+	cout << "B     B       J     A   A    R   R    NN   N    E" << endl;
+	cout << "BBBBBB        J     AAAAA    RRRRR    N N  N    EEE" << endl;
+	cout << "B     B    J  J     A   A    R R      N  N N    E" << endl;
+	cout << "BBBBBB     JJJJ     A   A    R   R    N   NN    EEEEE" << endl;
+
+
+
+
+
+
+}
