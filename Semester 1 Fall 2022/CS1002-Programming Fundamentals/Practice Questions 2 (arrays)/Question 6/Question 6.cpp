17cc4d6726e9404fe584a83ed7b65fe3ca4500af
faizanshabir123 <faizanshabir69@gmail.com>
2023-07-23 15:36:57 +0500

    Create Question 6.cpp

---
+++ Semester 1 Fall 2022/CS1002-Programming Fundamentals/Practice Questions 2 (arrays)/Question 6/Question 6.cpp
@@ -0,0 +1,61 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	int arr[100]; // declaring array to intially store all numbers
+	int DistinctArray[100]; // declaring array to store non-repeated numbers
+	int size = 0; // variable to store size of first array
+	int temp; // variable to store element of first array
+	int num; // variable to stored the entered numbers 
+	int size2; // variable to store the size of the second array
+
+	
+	cout << "Enter the size of array: ";
+	cin >> size; // inputting size of array
+	
+
+	cout << "Enter " << size << " integers: " << endl;
+
+	for (int i = 0; i < size; i++) // inputting elemnts in the first array
+	{
+		cin >> num;
+		arr[i] = num;
+	}
+
+	DistinctArray[0] = arr[0]; // assigning first element of fisrt array to first index of second array
+	size2 = 1; //updating size of second array
+
+	for (int i = 1; i < size; i++) // loop will run for the number of elemennts in the first array    
+	{
+		temp = arr[i];
+		bool flag = true;
+
+		for (int j = 0; j < size2; j++) // loop will run for the number of elements in second array
+		{
+			if (temp == DistinctArray[j])
+			{
+				flag = false; // flag will become fasle if the current element is a repeated element
+				break;
+
+			}
+		}
+
+		if (flag == true)
+		{
+			DistinctArray[size2] = temp; // storing a non-repeated element in the second array
+			size2++;
+		}
+	}
+
+
+	
+	cout << endl << endl << "The array after removing duplicates is: " << endl;
+
+	for (int j = 0; j < size2; j++) // outputing the non-repeated elements
+	{
+		cout << DistinctArray[j] << endl;
+	}
+
+
+
+}
