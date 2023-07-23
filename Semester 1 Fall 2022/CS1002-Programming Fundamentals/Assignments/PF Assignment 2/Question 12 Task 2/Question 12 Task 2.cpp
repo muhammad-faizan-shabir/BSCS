5fe406f3b3fd7cb12432f20a83ee68269501d56c
faizanshabir123 <faizanshabir69@gmail.com>
2023-07-23 15:20:49 +0500

    Create Question 12 Task 2.cpp

---
+++ Semester 1 Fall 2022/CS1002-Programming Fundamentals/Assignments/PF Assignment 2/Question 12 Task 2/Question 12 Task 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float X1;
+	float Y1;
+	float X2;
+	float Y2;
+	float X3;
+	float Y3;
+	float area;
+
+	cout << "Enter X coordinate of point 1: " << endl;
+	cin >> X1;
+	cout << "Enter Y coordinate of point 1: " << endl;
+	cin >> Y1;
+	cout << "Enter X coordinate of point 2: " << endl;
+	cin >> X2;
+	cout << "Enter Y coordinate of point 2: " << endl;
+	cin >> Y2;
+	cout << "Enter X coordinate of point 3: " << endl;
+	cin >> X3;
+	cout << "Enter Y coordinate of point 3: " << endl;
+	cin >>Y3;
+	
+	area = (0.5) * (((X1) * (Y2 - Y3)) + ((X2) * (Y3 - Y1)) + ((X3) * (Y1 - Y2)));
+
+	if(area==0)
+	{
+		cout << "Points 1, 2 and 3 are collinear" << endl;
+	}
+	else
+	{
+		cout << "Points 1, 2 and 3 are NOT collinear" << endl;
+	}
+}
