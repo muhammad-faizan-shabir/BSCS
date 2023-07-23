1fe9c79aeb10ece59690851a910129b240bc0b46
faizanshabir123 <faizanshabir69@gmail.com>
2023-07-23 15:10:30 +0500

    Create Question 5.cpp

---
+++ Semester 1 Fall 2022/CL1002-Programming Fundamentals - Lab/Lab Manuals/Lab Manual # 3/Question 5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char alphabet;
+	cout << "Enter an alphabet (capital or small)" << endl;
+	cin >> alphabet;
+	if ((alphabet == 'a') || (alphabet == 'A') || (alphabet == 'b') || (alphabet == 'B') || (alphabet == 'c') || (alphabet == 'C') || (alphabet == 'd') || (alphabet == 'D') || (alphabet == 'e') || (alphabet == 'E') || (alphabet == 'f') || (alphabet == 'F') || (alphabet == 'g') || (alphabet == 'G') || (alphabet == 'h') || (alphabet == 'H') || (alphabet == 'i') || (alphabet == 'I') || (alphabet == 'j') || (alphabet == 'J') || (alphabet == 'k') || (alphabet == 'K') || (alphabet == 'l') || (alphabet == 'L') || (alphabet == 'm') || (alphabet == 'M') || (alphabet == 'n') || (alphabet == 'N') || (alphabet == 'o') || (alphabet == 'O') || (alphabet == 'p') || (alphabet == 'P') || (alphabet == 'q') || (alphabet == 'Q') || (alphabet == 'r') || (alphabet == 'R') || (alphabet == 's') || (alphabet == 'S') || (alphabet == 't') || (alphabet == 'T') || (alphabet == 'u') || (alphabet == 'U') || (alphabet == 'v') || (alphabet == 'V') || (alphabet == 'w') || (alphabet == 'W') || (alphabet == 'x') || (alphabet == 'X') || (alphabet == 'y') || (alphabet == 'Y') || (alphabet == 'z') || (alphabet == 'Z'))
+	{
+		cout << "valid input"<<endl;
+		
+		if ((alphabet == 'a') || (alphabet == 'A') || (alphabet == 'e') || (alphabet == 'E') || (alphabet == 'i') || (alphabet == 'I') || (alphabet == 'o') || (alphabet == 'O') || (alphabet == 'u') || (alphabet == 'U')) 
+		{
+			cout << "This alphabet is a vowel" << endl;
+		}
+		else
+		{
+			cout << "This alphabet is consnant" << endl;
+		}
+	}
+	else
+	{
+		cout << "Invalid Input" << endl;
+	}
+
+
+}
