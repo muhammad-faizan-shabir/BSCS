9bfb6c0f8dd301474a8b97c7fdce2a60fba741d5
faizanshabir123 <faizanshabir69@gmail.com>
2024-01-12 18:21:06 +0500

    Create rand (working).asm

---
+++ Semester 3 Fall 2023/EE2003-Computer Organization and Assembly Language/Project/rand (working).asm
@@ -0,0 +1,54 @@
+[org 0x100]
+jmp start
+
+rand: dw 0
+randnum: dw 0
+
+; taking n as parameter, generate random number from 0 to n nad return in the stack
+randG:
+   push bp
+   mov bp, sp
+   pusha
+   cmp word [rand], 0
+   jne next
+
+  MOV     AH, 00h   ; interrupt to get system timer in CX:DX 
+  INT     1AH
+  inc word [rand]
+  mov     [randnum], dx
+  jmp next1
+
+  next:
+  mov     ax, 25173          ; LCG Multiplier
+  mul     word  [randnum]     ; DX:AX = LCG multiplier * seed
+  add     ax, 13849          ; Add LCG increment value
+  ; Modulo 65536, AX = (multiplier*seed+increment) mod 65536
+  mov     [randnum], ax          ; Update seed = return value
+
+ next1:xor dx, dx
+ mov ax, [randnum]
+ mov cx, [bp+4]
+ inc cx
+ div cx
+ 
+ mov [bp+6], dx
+ popa
+ pop bp
+ ret 2
+
+   
+
+
+start: 
+sub sp, 2
+push 79
+call randG
+pop dx
+
+sub sp,2
+push 79
+call randG
+pop dx
+
+mov ax, 0x4C00
+int 21h
