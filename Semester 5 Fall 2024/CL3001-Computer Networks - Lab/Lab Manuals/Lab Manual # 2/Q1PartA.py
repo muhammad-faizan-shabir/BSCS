473b73cc5599c5d95761a053333665034d4e7c35
faizanshabir123 <faizanshabir69@gmail.com>
2024-09-19 23:04:05 +0500

    Create Q1PartA.py

---
+++ Semester 5 Fall 2024/CL3001-Computer Networks - Lab/Lab Manuals/Lab Manual # 2/Q1PartA.py
@@ -0,0 +1,9 @@
+def main():
+    num= int(input("Enter number: "))
+
+    if (num%2==0):
+        print ("The number ",num, " is Even.")
+    else:
+        print ("The number ", num, " is Odd.")
+
+main()
